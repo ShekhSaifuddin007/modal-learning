405018f5facc257e7dfd72fa22f36231fa21a7a6
ShekhSaifuddin007 <saifsabbir4@gmail.com>
2021-03-29 23:18:08 +0600

    switch between two modal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,9 @@
         body {
             font-family: 'Nunito', sans-serif;
         }
+        [x-cloak] {
+            display: none !important;
+        }
     </style>
     <livewire:styles/>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <div class="container max-w-lg mx-auto bg-gray-300">
-        <header class="bg-blue-600 p-4">
+        <header class="bg-blue-600 p-4 flex justify-between">
             <h1 class="font-bold text-white">My Sites</h1>
+            <a href="#another">Open Modal</a>
         </header>
 
         <div class="grid grid-cols-12 p-4">
@@ -34,7 +35,7 @@
 
     {{-- Modal --}}
 
-    <x-confirmation-modal>
+    <x-confirmation-modal name="modal">
         <x-slot name="title">
             Are You Sure.?
         </x-slot>
@@ -44,7 +45,24 @@
         </x-slot>
 
         <x-slot name="footer">
-            <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button>
+{{--            <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button>--}}
+            <a href="#" class="bg-gray-400 hover:bg-gray-500 duration-200 px-4 py-2 rounded-md text-white transition uppercase mr-2">Cancel</a>
+            <x-button class="bg-blue-400 hover:bg-blue-500">Continue</x-button>
+        </x-slot>
+    </x-confirmation-modal>
+
+    <x-confirmation-modal name="another">
+        <x-slot name="title">
+            Are You Sure.?
+        </x-slot>
+
+        <x-slot name="body">
+            Another Modal
+        </x-slot>
+
+        <x-slot name="footer">
+            {{--            <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button>--}}
+            <a href="#modal" class="bg-gray-400 hover:bg-gray-500 duration-200 px-4 py-2 rounded-md text-white transition uppercase mr-2">Retry</a>
             <x-button class="bg-blue-400 hover:bg-blue-500">Continue</x-button>
         </x-slot>
     </x-confirmation-modal>
