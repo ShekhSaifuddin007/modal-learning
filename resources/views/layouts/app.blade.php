0e9d114bc655fe05a66cdbda331b70bec84617d3
ShekhSaifuddin007 <saifsabbir4@gmail.com>
2021-03-29 21:44:33 +0600

    modal complete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" xmlns:livewire="http://www.w3.org/1999/html">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,9 +17,10 @@
             font-family: 'Nunito', sans-serif;
         }
     </style>
+    <livewire:styles/>
 </head>
-<body class="antialiased">
-<div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center py-4 sm:pt-0">
+<body class="antialiased bg-gray-200">
+<div class="relative flex items-top sm:items-center py-4 sm:pt-0">
     @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
@@ -38,5 +39,7 @@
         {{ $slot }}
     </div>
 </div>
+
+<livewire:scripts/>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,56 @@
 <x-app-layout>
-    <h1 class="text-4xl font-semibold">Hello...............</h1>
+    <div class="container max-w-lg mx-auto bg-gray-300">
+        <header class="bg-blue-600 p-4">
+            <h1 class="font-bold text-white">My Sites</h1>
+        </header>
+
+        <div class="grid grid-cols-12 p-4">
+            <aside class="mr-6 col-span-3 text-sm">
+                <ul>
+                    <li>Link 1</li>
+                    <li>Link 2</li>
+                    <li>Link 3</li>
+                </ul>
+            </aside>
+
+            <main class="col-span-9 text-sm">
+                <p class="p-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto atque, dicta eaque eveniet explicabo harum, illum nesciunt optio quasi qui quis quod rem, sequi temporibus vitae voluptas. Expedita, quisquam!
+                </p>
+                <p class="p-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto atque, dicta eaque eveniet explicabo harum, illum nesciunt optio quasi qui quis quod rem, sequi temporibus vitae voluptas. Expedita, quisquam!
+                </p>
+                <p class="p-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto atque, dicta eaque eveniet explicabo harum, illum nesciunt optio quasi qui quis quod rem, sequi temporibus vitae voluptas. Expedita, quisquam!
+                </p>
+            </main>
+        </div>
+
+        <footer class="bg-blue-600 p-4 flex items-center justify-between text-xs text-white">
+            <h5>My Sites</h5>
+            <p>2021</p>
+        </footer>
+    </div>
+
+    {{-- Modal --}}
+
+    <div>
+        <div class="bg-gray-900 opacity-80 fixed inset-0"></div>
+        <div class="bg-white p-4 max-w-sm shadow-md h-48 m-auto rounded-md fixed inset-0">
+            <div class="flex h-full flex-col justify-between">
+                <header class="text-lg font-bold">
+                    Are You Sure.?
+                </header>
+
+                <main>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, non.
+                </main>
+
+                <footer>
+                    <button class="bg-gray-400 duration-200 hover:bg-gray-500 px-4 py-2 rounded-md text-white transition uppercase mr-2">Cancel</button>
+                    <button class="bg-blue-400 duration-200 hover:bg-blue-500 px-4 py-2 rounded-md text-white transition uppercase">Continue</button>
+                </footer>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
